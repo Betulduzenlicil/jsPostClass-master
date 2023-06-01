78e2d806bcac58b0f8f933c6ce1260b1d3b70da9
Betül <betulcil.25@gmail.com>
2023-06-01 23:07:35 +0300

    doc

---
+++ jsPostClass15_createElement-events/events.js
@@ -0,0 +1,77 @@
+//* ==============================================
+//*                  EVENTS
+//* ===============================================
+
+console.log("****** EVENTS *******")
+
+// document.querySelector("header h1").onmouseover = function(){
+//     document.querySelector("header h1").style.color = "red"
+// }
+
+//? Alternative Way
+
+const heading1 = document.querySelector("header h1")
+
+heading1.onmouseover = function () {
+    heading1.style.color = "red"
+}
+
+heading1.onmouseout = function () {
+    heading1.style.color = "blue"
+}
+
+
+const printHello = ()=>{
+    document.write("Hello")
+}
+
+
+//? onload eventi html ve css kodlarının render edilmesi akabinde çalışır
+window.onload = printHello()
+
+window.addEventListener("load",()=>{
+    document.getElementById("input").focus()
+})
+
+
+const addBtn =document.querySelector("#btn")
+
+const ul = document.createElement("ul")
+document.querySelector(".item-list").before(ul)
+
+
+
+addBtn.addEventListener("click", (e)=>{
+    // console.log("Btn Clicked")
+    // console.log(e)
+    // console.log(e.target)
+    // console.log(e.target.value)
+
+    if(!input.value){
+        alert("please enter a comment")
+    }else{
+
+    const input = document.querySelector("#input")
+    const li = document.createElement("li")
+    ul.appendChild(li)
+    const textLi = document.createTextNode(input.value)
+    li.appendChild(textLi)
+    input.value =""
+    input.focus()
+
+}
+})
+
+// İNPUTUN ÜZERİNDE İKEN BİR KLAVYE TUŞUNA BASILINCA ÇALIŞ
+document.querySelector("#input").addEventListener("keydown", (e)=>{
+    console.log(e.code)
+    console.log(e.keyCode)
+
+
+    //BASILAN TUŞ ENTER İSE
+    if (e.code === "Enter"){
+        addBtn.click() // add butonunun tıklama fonksiyonunu çalıştırırım
+    }
+})
+
+
