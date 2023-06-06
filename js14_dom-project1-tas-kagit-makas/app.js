4389f9e68cbd370e4b4b7ca93bc77f453aa14fb1
Betül <betulcil.25@gmail.com>
2023-06-06 15:34:30 +0300

    asd

--- js14_dom-project1-tas-kagit-makas/app.js
+++ js14_dom-project1-tas-kagit-makas/app.js
@@ -11,38 +11,59 @@ const yourChoiceDiv = document.getElementById("your-choice")
 //* ------- Variables ------- */
 
 
+
 //* ------- Event Listeners ------- */
-rockImg.addEventListener("click", ()=>{
-    const image = document.createElement("img")
-    image.src = "./assets/rock.png"
-    image.alt="rock"
-    yourChoiceDiv.appendChild(image)
+selectionArticle.addEventListener('click', (e) => {
+    
+    if (e.target.id=== "rock"||
+        e.target.id=== "paper"||
+        e.target.id=== "scissor") {
+        Image.src=`./assets/${e.target.id}.png`
+        userSelectImg.src = `./assets/${e.target.id}.png`
+        userSelectImg.alt = e.target.id
+        yourChoiceDiv.appendChild(userSelectImg)
+        
+    }
+});
 
-
-    //? inner HTML
-    // yourChoiceDiv.innerHTML = `<img src = "./assets/rock.png" alt="rock"/>`
-
-})
-
-paperImg.addEventListener("click", ()=>{
-    const image = document.createElement("img")
-    image.src = "./assets/paper.png"
-    image.alt="paper"
-    yourChoiceDiv.appendChild(image)
-
-
-
-})
-
-scissorImg.addEventListener("click", ()=>{
-    const image = document.createElement("img")
-    image.src = "./assets/scissor.png"
-    image.alt="scissor"
-    yourChoiceDiv.appendChild(image)
-
-
-
-})
+const createPcSelection =() =>{
+    const pcArr = ["rock", "papaer", "scissor"]
+    const pcRandom = psArr[Math.floor(Math.random()*3)]
+}
 
 
 //* ------- Functions ------- */
+
+
+// let image = document.createElement("img")
+// rockImg.addEventListener("click", ()=>{
+//     const image = document.createElement("img")
+//     image.src = "./assets/rock.png"
+//     image.alt="rock"
+//     yourChoiceDiv.appendChild(image)
+
+
+    //? inner HTML yolu ile oluşturulabilir
+    // yourChoiceDiv.innerHTML = `<img src = "./assets/rock.png" alt="rock"/>`
+
+// })
+
+// paperImg.addEventListener("click", ()=>{
+//     const image = document.createElement("img")
+//     image.src = "./assets/paper.png"
+//     image.alt="paper"
+//     yourChoiceDiv.appendChild(image)
+
+
+
+// })
+
+// scissorImg.addEventListener("click", ()=>{
+//     const image = document.createElement("img")
+//     image.src = "./assets/scissor.png"
+//     image.alt="scissor"
+//     yourChoiceDiv.appendChild(image)
+
+
+
+// })
